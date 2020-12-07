bc7f7a183c9f6988edb16bd66e336f23910aa39d
190161 <190161xh@yse-c.net>
2020-12-07 11:30:07 +0900

    add files.

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const express = require('express');
 //サーバーの作成
 const app = express();
 
+//JSON対応
+app.use(express.json());
+
+// URLエンコードされたデータを解析する
+app.use(express.urlencoded({extended: true}));
+
+//XSS　クロスドメイン対策
+app.use((req, res, next) => {  
+    console.log('middleware: all. url: ${req.url}');
+    
+    //CROS設定: 全てのドメインに対して許可
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    //次の処理
+    next();
+});
+
+app.post('/', (req, res) => {
+    let result = {
+        message: req.body.message,
+    };
+    res.send(result);
+});
+
 //ルーティング
 //Webルートにリクエストされたらレスポンス
 app.get("/", (req, res) => {
